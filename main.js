f6c21d7f6f675bfb19fcd7ab8e10681047e2f870
isabela-tassia <isabelatassiasilva@gmail.com>
2023-10-05 09:51:42 -0300

    adding stars

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+import './style.css';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -26,6 +28,23 @@ const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
 
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar(){
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+  .fill()
+  .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+
+}
+
+Array(200).fill().forEach(addStar);
 
 function animate() {
 	requestAnimationFrame( animate );
@@ -33,6 +52,8 @@ function animate() {
 	torus.rotation.x += 0.01;
 	torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+
+  controls.update();
 
 	renderer.render( scene, camera );
 }
